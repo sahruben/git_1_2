876761158a1a1f3266d0cde4f0c30a21f0b6f12e
Ruben Sahakyan <sahakyanruben1981@gmail.com>
2023-12-26 12:38:47 +0100

    add Car class

---
+++ src/Car.java
@@ -0,0 +1,9 @@
+public class Car extends Vehicle {
+
+    private  String model;
+
+    public  Car(String brand, int year, String model){
+        super(brand, year);
+        this.model =model;
+    }
+}
